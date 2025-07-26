626832ebf32b3e6b18dfdbfd3d5bb0114cd96143
david oh <ghkvud5614@gmail.com>
2025-07-26 15:59:47 +0900

    second commit

--- proj/settings.py
+++ proj/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -74,13 +77,36 @@ WSGI_APPLICATION = "proj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET NAMES utf8mb4",
+        },
     }
 }
 
+# 테스트 환경에서는 SQLite로 대체
+if os.environ.get("TEST"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,125 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Article,
+    CuisineType,
+    Region,
+    Restaurant,
+    RestaurantCategory,
+    RestaurantImage,
+    RestaurantMenu,
+    Review,
+    ReviewImage,
+    SocialChannel,
+    Tag,
+)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "show_at_index",
+        "is_published",
+        "created_at",
+        "modified_at",
+    ]
+
+    fields = ["title", "preview_image", "content", "show_at_index", "is_published"]
+
+    search_fields = ["title"]
+    list_filter = ["show_at_index", "is_published"]
+    date_hierarchy = "created_at"
+    actions = ["make_published"]
+
+    @admin.action(description="선택한 컬럼을 공개상태로 변경합니다.")
+    def make_published(self, request, queryset):
+        queryset.update(is_published=True)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    fields = ["name"]
+    search_fields = ["name"]
+
+
+class RestaurantMenuInline(admin.TabularInline):
+    model = RestaurantMenu
+    extra = 1
+
+
+class RestaurantImageInline(admin.TabularInline):
+    model = RestaurantImage
+    extra = 1
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "branch_name",
+        "is_closed",
+        "phone",
+        "rating",
+        "rating_count",
+        "region",
+    ]
+    fields = [
+        "name",
+        "branch_name",
+        "category",
+        "is_closed",
+        "phone",
+        "latitude",
+        "longitude",
+        "tags",
+    ]
+    readonly_fields = ["rating", "rating_count", "region"]
+    search_fields = ["name", "branch_name"]
+    list_filter = ["tags"]
+    autocomplete_fields = ["tags"]
+    inlines = [RestaurantMenuInline, RestaurantImageInline]
+
+    def get_inline_instances(self, request, obj=None):
+        return obj and super().get_inline_instances(request, obj) or []
+
+
+@admin.register(RestaurantCategory)
+class RestaurantCategoryIAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    fields = ["cuisine_type", "name"]
+
+
+@admin.register(CuisineType)
+class CuisineTypeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+
+
+class ReviewImageInline(admin.TabularInline):
+    model = ReviewImage
+    extra = 1
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["id", "restaurant_name", "author", "rating", "content_partial"]
+    inlines = [ReviewImageInline]
+
+    def get_inline_instances(self, request, obj=None):
+        return obj and super().get_inline_instances(request, obj) or []
+
+
+@admin.register(SocialChannel)
+class SocialChannelAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    fields = ["name"]
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("sido", "sigungu", "eupmyeondong")
+    search_fields = ("sido", "sigungu", "eupmyeondong")
